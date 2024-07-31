5e68edf21a48c5676044f722fc61aaebe8b7674b
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-08-01 07:06:35 +0800

    Updated

--- resources/views/teacher/layouts/app.blade.php
+++ resources/views/teacher/layouts/app.blade.php
@@ -119,7 +119,31 @@
                 </div>
 
                 <div class="p-3 {{ request()->is('teacher/my_students') ? 'bg-blue-50 text-blue-500' : 'hover:bg-blue-50 hover:text-blue-500 text-gray-700' }} rounded">
-                    <a href="" class="text-sm flex items-center justify-start gap-3"><i class="fa-solid fa-list"></i>My Students</a>
+                    <div class="relative inline-block text-left w-full">
+                        <div class="w-full">
+                            <button type="button" class="text-sm flex items-center justify-start gap-3 w-full" id="menu-button-2" aria-expanded="true" aria-haspopup="true">
+                                <i class="fa-solid fa-users"></i>
+                                My Students
+                                <svg class="-mr-1 h-5 w-5 text-gray-400" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                                    <path fill-rule="evenodd" d="M5.23 7.21a.75.75 0 011.06.02L10 11.168l3.71-3.938a.75.75 0 111.08 1.04l-4.25 4.5a.75.75 0 01-1.08 0l-4.25-4.5a.75.75 0 01.02-1.06z" clip-rule="evenodd" />
+                                </svg>
+                            </button>
+                        </div>
+
+                        <div class="absolute right-0 z-10 mt-2 w-56 origin-top-right rounded-md bg-white shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none hidden" id="dropdown-menu-2" role="menu" aria-orientation="vertical" aria-labelledby="menu-button-2" tabindex="-1">
+                            <div role="none">
+                                @if ($handleSubjects->isEmpty())
+                                    
+                                        <div class="p-4 text-gray-600 text-sm"><p class="text-rose-600">No students found.</p></div>
+                                    
+                                @else
+                                    @foreach ($handleSubjects as $gradeHandle)
+                                           <a href="{{ route('teacher.subject_list', ['id' => $gradeHandle->id])}}" title="Click to view" class="hover:bg-blue-50 hover:text-blue-500 text-gray-700 block px-4 py-2 text-sm flex items-center justify-start gap-3" role="menuitem" tabindex="-1" id="menu-item-0"><i class="fa-solid fa-list-ul"></i>Grade {{ $gradeHandle->grade }} - {{ $gradeHandle->strand }}</a>
+                                    @endforeach
+                                @endif
+                            </div>
+                        </div>
+                    </div>
                 </div>
 
                 <div class="p-3 {{ request()->is('teacher/class_history') ? 'bg-blue-50 text-blue-500' : 'hover:bg-blue-50 hover:text-blue-500 text-gray-700' }} rounded">
@@ -335,6 +359,20 @@
                 if (!$(event.target).closest('#menu-button, #dropdown-menu').length) {
                     $('#dropdown-menu').addClass('hidden');
                     $('#menu-button').attr('aria-expanded', 'false');
+                }
+            });
+
+
+            $('#menu-button-2').on('click', function() {
+                const expanded = $(this).attr('aria-expanded') === 'true';
+                $(this).attr('aria-expanded', !expanded);
+                $('#dropdown-menu-2').toggleClass('hidden');
+            });
+
+            $(document).on('click', function(event) {
+                if (!$(event.target).closest('#menu-button-2, #dropdown-menu-2').length) {
+                    $('#dropdown-menu-2').addClass('hidden');
+                    $('#menu-button-2').attr('aria-expanded', 'false');
                 }
             });
         });
