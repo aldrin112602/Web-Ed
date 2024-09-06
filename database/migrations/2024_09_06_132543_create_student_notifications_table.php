160fa67f1db20e8dddf76274315e356cad46bca6
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-09-06 21:34:35 +0800

    Updated migration

--- database/migrations/2024_09_06_132543_create_student_notifications_table.php
+++ database/migrations/2024_09_06_132543_create_student_notifications_table.php
@@ -13,7 +13,17 @@ return new class extends Migration
     {
         Schema::create('student_notifications', function (Blueprint $table) {
             $table->id();
+            $table->string('type')->nullable();
+            $table->unsignedBigInteger('user_id');
+            $table->string('title')->nullable();
+            $table->text('message');
+            $table->boolean('is_seen')->default(false);
+            $table->string('url')->nullable();
+            $table->string('icon')->nullable();
+            $table->enum('priority', ['low', 'medium', 'high'])->default('low');
             $table->timestamps();
+
+            $table->foreign('user_id')->references('id')->on('student_accounts')->onDelete('cascade');
         });
     }
 
