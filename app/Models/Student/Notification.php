a2beb8af8a957256e2dc4d6639c02cf729d0d587
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-09-06 20:40:38 +0800

    Added Notification

---
+++ app/Models/Student/Notification.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Student;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Notification extends Model
+{
+    use HasFactory;
+}
