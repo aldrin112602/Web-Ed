f838ba7d4fcf3ada4a45bace5652abd700ca4d4b
Aldrin Caballero <caballeroaldrin02@gmail.com>
2024-10-18 18:58:23 +0800

    Fixed route

--- routes/root/public.php
+++ routes/root/public.php
@@ -5,11 +5,27 @@ use App\Http\Controllers\{
     PublicController,
     FaceRecognitionController
 };
-
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
+
+    $guards = [
+        'admin' => 'admin.dashboard',
+        'teacher' => 'teacher.dashboard',
+        'student' => 'student.dashboard',
+        'guidance' => 'guidance.dashboard'
+    ];
+
+    foreach ($guards as $guard => $route) {
+        if (Auth::guard($guard)->check()) {
+            return redirect()->route($route);
+        }
+    }
+
+
     return view('welcome');
 });
+
 
 
 Route::get('/face_recognition', [FaceRecognitionController::class, 'showFaceRecognition'])->name('face.recognition');
